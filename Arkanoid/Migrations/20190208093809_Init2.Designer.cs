9c9b53d16d90391b3f15e59bd8563d32f6c8e1f1
Aleksey Grigoryev <drummaprod@gmail.com>
2019-02-08 13:57:19 +0400

    feature: CRUD

--- Arkanoid/Migrations/20190208093809_Init2.Designer.cs
+++ Arkanoid/Migrations/20190208093809_Init2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Arkanoid.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Arkanoid.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190208093809_Init2")]
+    partial class Init2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
