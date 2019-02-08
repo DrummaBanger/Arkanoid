6cd38de092170a16bdbd7ca9adb318c8b2be4702
Aleksey Grigoryev <drummaprod@gmail.com>
2019-02-08 13:32:37 +0400

    feature: connect database

--- Arkanoid/Startup.cs
+++ Arkanoid/Startup.cs
@@ -36,7 +36,7 @@ namespace Arkanoid
             });
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
+                options.UseNpgsql(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
