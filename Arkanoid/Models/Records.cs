8005557cf5934b09a1b1daff7121e6543b762791
Aleksey Grigoryev <drummaprod@gmail.com>
2019-02-13 12:57:25 +0400

    feature: edit view model

--- Arkanoid/Models/Records.cs
+++ Arkanoid/Models/Records.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +10,15 @@ namespace Arkanoid.Models
 {
     public class Records
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [ScaffoldColumn(false)]
         public int UserID { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "№ рекорда")]
         public int RecordID { get; set; }
+        [Display(Name = "Имя игрока")]
         public string UserName { get; set; }
+        [Display(Name = "Счет")]
         public int UserScore { get; set; }
     }
 }
